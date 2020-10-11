610b7759176f6012e9fe6df0ca28a99ff4d78aa2
nieyangyang712 <290385211@qq.com>
2020-10-11 16:00:34 +0800

    update

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+// pages/index/index.js
+Page({
+  bendi() {
+    wx.navigateTo({
+      url: '../canvas/canvas',
+    })
+  },
+  weixin() {
+    wx.navigateTo({
+      url: '../canvas2/canvas2',
+    })
+  }
+})
