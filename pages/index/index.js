1789371a35409bb1e0966f39c7c319dccf8855f2
nieyangyang712 <290385211@qq.com>
2020-10-11 17:32:32 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,26 @@
 // pages/index/index.js
 Page({
+  //本地图片
   bendi() {
     wx.navigateTo({
       url: '../canvas/canvas',
     })
   },
+  // 微信图像
   weixin() {
     wx.navigateTo({
       url: '../canvas2/canvas2',
     })
+  },
+  //第一步设置可被分享
+  onShareAppMessage:function(res){
+    return {title:'我是传统分享的标题'}
+  },
+  //第二步： 设置分享到朋友圈的标题
+  onShareTimeline:function(res){
+    return {
+      title: "转发到朋友圈",
+      query: "我是携带的参数"
+    }
   }
 })
